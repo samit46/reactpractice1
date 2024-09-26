8bd6b5e287614f4ae168657716c12334af8c97f7
samit46 <samitshaikh46@gmail.com>
2024-09-26 23:45:06 +0530

    react

--- practise/src/App.js
+++ practise/src/App.js
@@ -3,11 +3,35 @@ import './App.css';
 import Profile from './components/Profile';
 
 function App() {
+
+  const profiles =[
+    {
+    name :"samit",
+    img :"https://t3.ftcdn.net/jpg/02/16/60/42/360_F_216604243_4yvQhIudgknAWu04Ob7aSOu6BdIfhBNb.jpg",
+    about :"i am  web designer",
+  },
+    {
+    name :"huzaif",
+    img :"https://imgd.aeplcdn.com/370x208/n/cw/ec/132513/7-series-exterior-right-front-three-quarter-3.jpeg?isig=0&q=80",
+    about :"i am  web developer",
+  },
+    {
+    name :"rehan",
+    img :"https://img.freepik.com/free-vector/blue-sports-car-isolated-white-vector_53876-67354.jpg",
+    about :"i am  student",
+  }
+]
   return (
-    
+   
     <div>
       <h1>profile</h1>
-      <Profile name={"samit"} pic={"https://t3.ftcdn.net/jpg/02/16/60/42/360_F_216604243_4yvQhIudgknAWu04Ob7aSOu6BdIfhBNb.jpg"} us={"   i am  web designer"}/>
+    
+    {profiles.map ((profile ,key) =>{
+    return<Profile key={key} name={profile.name} pic={profile.img} us={profile.about}/>
+})}
+
+
+
 
     </div>
 
--- practise/src/components/profile.js
+++ practise/src/components/profile.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Profile = ({pic,name,us}) => {
+  
     return(
         
       <div className='profile'>
-        <p className='name'>{name}</p>
+        <p className='name'>my name is {name}</p>
         <img className='profile-image'src={pic} alt={pic}></img>
         <p className='about'>{us}</p>
       </div>
