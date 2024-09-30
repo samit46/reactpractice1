90ed7e14634617b598d7859da4d939dbb9754727
samit46 <samitshaikh46@gmail.com>
2024-09-30 19:34:00 +0530

    30th september

--- practise/src/App.js
+++ practise/src/App.js
@@ -1,38 +1,46 @@
 
 import './App.css';
 import Profile from './components/Profile';
+import Card from 'react-bootstrap/Card';
+
 
 function App() {
 
   const profiles =[
     {
     name :"samit",
-    img :"https://t3.ftcdn.net/jpg/02/16/60/42/360_F_216604243_4yvQhIudgknAWu04Ob7aSOu6BdIfhBNb.jpg",
-    about :"i am  web designer",
+    img :"https://avatars.githubusercontent.com/u/182517915?v=4&size=64",
+    about :"i am  web developer",
+    profileurl :"https://github.com/samit46",
   },
     {
     name :"huzaif",
     img :"https://imgd.aeplcdn.com/370x208/n/cw/ec/132513/7-series-exterior-right-front-three-quarter-3.jpeg?isig=0&q=80",
     about :"i am  web developer",
+    profileurl:"https://github.com/Rehandossa"
   },
     {
     name :"rehan",
     img :"https://img.freepik.com/free-vector/blue-sports-car-isolated-white-vector_53876-67354.jpg",
     about :"i am  student",
+    profileurl:"https://github.com/Rehandossa"
+  }, 
+  {
+    name :"muizz",
+    img :"https:encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1M7qh5Onl1a1stLFy9R9q8EOwjdzn4naI2Q&s",
+    about :"i am a death",
+    profilesurl:"https://github.com/samit46"
   }
 ]
   return (
    
-    <div>
-      <h1>profile</h1>
+    <div className='container mt-5'>
+       
+      <h1>Github profiles</h1>
     
     {profiles.map ((profile ,key) =>{
-    return<Profile key={key} name={profile.name} pic={profile.img} us={profile.about}/>
+    return<Profile key={key} name={profile.name} pic={profile.img} us={profile.about}url={profiles.profileurl}/>
 })}
-
-
-
-
     </div>
 
   );
--- practise/src/components/profile.js
+++ practise/src/components/profile.js
@@ -1,14 +1,38 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 
-const Profile = ({pic,name,us}) => {
+import Card from 'react-bootstrap/Card';
+
+
+const Profile = ({pic,name,us,url}) => {
   
     return(
-        
-      <div className='profile'>
-        <p className='name'>my name is {name}</p>
-        <img className='profile-image'src={pic} alt={pic}></img>
-        <p className='about'>{us}</p>
-      </div>
+   
+       
+       <Card className='mb-5' style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={pic}/>
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          {us}
+        </Card.Text>
+        <Card.Link href={url} target="blank">   <Button variant="primary">visit profile</Button></Card.Link>
+      </Card.Body>
+    </Card>
+      
+      
+   
+    
     )
 }
 export default Profile;
+
+
+
+
+
+// <div className='profile'>
+//         <p className='name'>my name is {name}</p>
+//         <img className='profile-image'src={pic} alt={pic}></img>
+//         <p className='about'>{us}</p>
+//         </div>
