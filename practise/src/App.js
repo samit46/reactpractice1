f196fc5cfc15cd7b332dcf5e37a49d1472f94362
samit46 <samitshaikh46@gmail.com>
2024-10-09 18:22:33 +0530

    halo

--- practise/src/App.js
+++ practise/src/App.js
@@ -4,6 +4,7 @@ import Profile from "./components/Profile";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import React, { useState, useEffect } from "react";
+import { Alert } from "bootstrap";
 function App() {
  
  const intialData = {
@@ -11,13 +12,15 @@ function App() {
   number2 : "",
   result : ""
  }
+ const Result = ""
+
   const [data , setData] = useState({intialData})
   const updateNumber1 = 
     
       (e)=>{
         setData( {
           ...data,
-          parseFloat: e.target.value
+         number1:parseFloat(e.target.value)
         })
       }
       const updateNumber2 = 
@@ -25,34 +28,77 @@ function App() {
       (e)=>{
         setData( {
           ...data,
-          parseFloat: e.target.value
+         number2:parseFloat(e.target.value)
         })
       }
-      const updateresult = 
-      (e)=>{
-       
+      const updateResult = 
+      (action)=>{
+      if (action == 'add') { 
+        setData({...data,result: data.number1 + data.number2})
+        
+      }
+       else if (action == 'sub') {
+       if (data.number1 >= data.number2) {
+        setData({...data,result:data.number1-data.number2})
+       }
+       else{
+        alert(data.number1 + " should be greater than " + data.number2  );
+       }
+      }
+       else if (action == 'mul') {
+        setData({...data,result:data.number1 * data.number2})
+      }
+       else if (action == 'div') {
+        setData({...data,result: data.number1 / data.number2 })
+      }
+      else {
+        setData(intialData)
+
+      }
+      
       } 
 
+      const [history , sethistory] = useState([])
+      const previousHistory =
+       (e)=>{
+        setData({
+          ...data,
+          result:e.target.value
+        })
+
+       }
+       console.log(previousHistory);
+       
 
   
  
   
   return (
-    <div className="container">
-      <h1>result : {data.Result}</h1>
-      <label  for="number1"> number1</label>
-      <input className="m-5" type="number" placeholder="ENTER YOUR  NUMBER" id="number1" onChange={updateNumber1}/>
-      <label for ="number2"> number2</label>
-      <input  className="m-5"type="number" placeholder="ENTER YOUR  NUMBER" id="number2" onChange={updateNumber2}/> 
-     
+    <div className="container row">
+     <div className="container col-md-8 md">
+     <h1>result :{data.result}</h1>
+      <label htmlFor="number1">Number 1:</label>
+      <input className="m-5" type="number" placeholder="Enter your number" id="number1" value={data.number1} onChange={updateNumber1} />
+      <label htmlFor="number2">Number 2:</label>
+      <input className="m-5" type="number" placeholder="Enter your number" id="number2" value={data.number2} onChange={updateNumber2} />
+
       
         <div>
-        <input type="button" className="m-2 p-3" value={"+"} onClick={() =>('add')}/>
-        <input type="button" className="m-2 p-3" value={"-"} onClick={() =>('sub')}/>
-        <input type="button" className="m-2 p-3" value={"*"} onClick={() =>{'mul'}}/>
-        <input type="button" className="m-2 p-3" value={"/"} onClick={() =>{'div'}}/>
-        <input type="button" className="m-2 p-3" value={"c"}/>
+        <input type="button" className="m-2 p-3" value={"+"} onClick={() => updateResult('add')}/>
+        <input type="button" className="m-2 p-3" value={"-"} onClick={() => updateResult('sub')}/>
+        <input type="button" className="m-2 p-3" value={"*"} onClick={() => updateResult('mul')}/>
+        <input type="button" className="m-2 p-3" value={"/"} onClick={() => updateResult('div')}/>
+        <input type="button" className="m-2 p-3" value={"c"} onClick={() => updateResult('c')}/>
         </div>
+     </div>
+     <div className="container col-md-4">
+      <h1>history</h1>
+      <ol>
+        <li> {history}</li>
+      </ol>
+      <input type="button" value="clear"/>
+
+     </div>
       
     </div>
  
