ade3e1894fb79bddb7468770c49b20051cd16c47
samit46 <samitshaikh46@gmail.com>
2024-10-10 19:33:18 +0530

    calc

--- practise/src/App.js
+++ practise/src/App.js
@@ -1,205 +1,120 @@
-
 import "./App.css";
-import Profile from "./components/Profile";
+import Calculator from "./components/calculator";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import React, { useState, useEffect } from "react";
 import { Alert } from "bootstrap";
+import Hello from "./components/hello";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 function App() {
- 
- const intialData = {
-  number1 : "",
-  number2 : "",
-  result : ""
- }
- const Result = ""
-
-  const [data , setData] = useState({intialData})
-  const updateNumber1 = 
-    
-      (e)=>{
-        setData( {
-          ...data,
-         number1:parseFloat(e.target.value)
-        })
-      }
-      const updateNumber2 = 
-    
-      (e)=>{
-        setData( {
-          ...data,
-         number2:parseFloat(e.target.value)
-        })
-      }
-      const updateResult = 
-      (action)=>{
-      if (action == 'add') { 
-        setData({...data,result: data.number1 + data.number2})
-        
-      }
-       else if (action == 'sub') {
-       if (data.number1 >= data.number2) {
-        setData({...data,result:data.number1-data.number2})
-       }
-       else{
-        alert(data.number1 + " should be greater than " + data.number2  );
-       }
-      }
-       else if (action == 'mul') {
-        setData({...data,result:data.number1 * data.number2})
-      }
-       else if (action == 'div') {
-        setData({...data,result: data.number1 / data.number2 })
-      }
-      else {
-        setData(intialData)
-
-      }
-      
-      } 
-
-      const [history , sethistory] = useState([])
-      const previousHistory =
-       (e)=>{
-        setData({
-          ...data,
-          result:e.target.value
-        })
-
-       }
-       console.log(previousHistory);
-       
-
-  
- 
-  
-  return (
-    <div className="container row">
-     <div className="container col-md-8 md">
-     <h1>result :{data.result}</h1>
-      <label htmlFor="number1">Number 1:</label>
-      <input className="m-5" type="number" placeholder="Enter your number" id="number1" value={data.number1} onChange={updateNumber1} />
-      <label htmlFor="number2">Number 2:</label>
-      <input className="m-5" type="number" placeholder="Enter your number" id="number2" value={data.number2} onChange={updateNumber2} />
-
-      
-        <div>
-        <input type="button" className="m-2 p-3" value={"+"} onClick={() => updateResult('add')}/>
-        <input type="button" className="m-2 p-3" value={"-"} onClick={() => updateResult('sub')}/>
-        <input type="button" className="m-2 p-3" value={"*"} onClick={() => updateResult('mul')}/>
-        <input type="button" className="m-2 p-3" value={"/"} onClick={() => updateResult('div')}/>
-        <input type="button" className="m-2 p-3" value={"c"} onClick={() => updateResult('c')}/>
-        </div>
-     </div>
-     <div className="container col-md-4">
-      <h1>history</h1>
-      <ol>
-        <li> {history}</li>
-      </ol>
-      <input type="button" value="clear"/>
-
-     </div>
-      
-    </div>
- 
-    
-
+  return(
+    <Navbar expand="lg" className="bg-body-tertiary">
+    <Container>
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="./components/hello">Calculator</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+          
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   )
 }
-{/* <div className='container mt-5'> */}
-       
+
+
+  /* <div className='container mt-5'> */
+
+
 //    <div className='row'>
 //    <h1>Github profiles</h1>
-    
-//     {profiles.map ((profile ,key) =>{
-//     return<Profile key={key} profile={profile}/>
-// })}
+
 //    </div>
 //     </div>
 
- //   <div className="container">
-  //     {
-  //       isEmpty ?
-  //         <h1>No Data</h1>
-  //       :
-  //       <>
-  //       {
-  //         data.name != '' ?<h1>Name: {data.name}</h1> : <></>
-  //       }
-  //       {
-  //         data.age != '' ?<h1>Age: {data.age}</h1> : <></>
-          
-  //       }
-  //       {
-  //         data.email != '' ?<h1>Email: {data.email}</h1> : <></>
-          
-  //       }
-  //         {
-  //         data.city != '' ?<h1>city: {data.city}</h1> : <></>
-          
-  //       }
-  //       </>
-  //     }
-  // <input type="text" placeholder="name" onChange={updateName}/>
-  // <input type="number" placeholder="age" onChange={updateAge}/>
-  // <input type="email" placeholder="email" onChange={upemail}/>
-  // <input type="text" placeholder="city" onChange={updateCity}/>
+//   <div className="container">
+//     {
+//       isEmpty ?
+//         <h1>No Data</h1>
+//       :
+//       <>
+//       {
+//         data.name != '' ?<h1>Name: {data.name}</h1> : <></>
+//       }
+//       {
+//         data.age != '' ?<h1>Age: {data.age}</h1> : <></>
 
-  
-  //   </div>
+//       }
+//       {
+//         data.email != '' ?<h1>Email: {data.email}</h1> : <></>
 
+//       }
+//         {
+//         data.city != '' ?<h1>city: {data.city}</h1> : <></>
 
-  // const [isEmpty, setIsEmpty] = useState(true); 
+//       }
+//       </>
+//     }
+// <input type="text" placeholder="name" onChange={updateName}/>
+// <input type="number" placeholder="age" onChange={updateAge}/>
+// <input type="email" placeholder="email" onChange={upemail}/>
+// <input type="text" placeholder="city" onChange={updateCity}/>
 
-  // const updateName = 
-    
-  //     (e)=>{
-  //       setData( {
-  //         ...data,
-  //         name: e.target.value
-  //       })
-  //       setIsEmpty(e.target.value == '' && data.age == "" &&  data.email == ''&&  data.city == '' ? true : false)
-  //     }
-    
+//   </div>
 
-  // const updateAge =
-    
-  //     (e)=>{
-  //       setData({
-  //         ...data,
-  //         age: e.target.value
-  //       })
-        
-  //       setIsEmpty(e.target.value == ''  &&  data.name == ''&&  data.email == ''&&  data.city == '' ? true : false)
- 
-  //       }
-  
-  // const upemail = 
-    
-   
-  //     (e)=>{
-  //       setData({
-  //         ...data,
-  //         email:e.target.value
-  //       })
-        
-  //       setIsEmpty(e.target.value == '' && data.age == "" &&  data.name == ''&&  data.city == '' ? true : false)
- 
-  //     }
-  //  const updateCity =   
-  //  (e)=>{
-  //   setData( {
-  //     ...data,
-  //     city: e.target.value
-  //   })
-  //   setIsEmpty(e.target.value == '' && data.age == "" &&  data.email == '' &&  data.name == ''? true : false)
-  // }
-    
+// const [isEmpty, setIsEmpty] = useState(true);
 
-   // useEffect(()=>{
-  //   fetch('/data/profiles.json')
-  //   .then(Response => Response.json())
-  //   .then(data => setProfiles(data))
-  //   .catch(error =>console.error('error fetching the profile',error));
-  // },[]);
+// const updateName =
+
+//     (e)=>{
+//       setData( {
+//         ...data,
+//         name: e.target.value
+//       })
+//       setIsEmpty(e.target.value == '' && data.age == "" &&  data.email == ''&&  data.city == '' ? true : false)
+//     }
+
+// const updateAge =
+
+//     (e)=>{
+//       setData({
+//         ...data,
+//         age: e.target.value
+//       })
+
+//       setIsEmpty(e.target.value == ''  &&  data.name == ''&&  data.email == ''&&  data.city == '' ? true : false)
+
+//       }
+
+// const upemail =
+
+//     (e)=>{
+//       setData({
+//         ...data,
+//         email:e.target.value
+//       })
+
+//       setIsEmpty(e.target.value == '' && data.age == "" &&  data.name == ''&&  data.city == '' ? true : false)
+
+//     }
+//  const updateCity =
+//  (e)=>{
+//   setData( {
+//     ...data,
+//     city: e.target.value
+//   })
+//   setIsEmpty(e.target.value == '' && data.age == "" &&  data.email == '' &&  data.name == ''? true : false)
+// }
+
+// useEffect(()=>{
+//   fetch('/data/profiles.json')
+//   .then(Response => Response.json())
+//   .then(data => setProfiles(data))
+//   .catch(error =>console.error('error fetching the profile',error));
+// },[]);
 export default App;
---
+++ practise/src/components/hello.js
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from "react";
+
+
+function Hello() {
+  const intialData = {
+    number1: "",
+    number2: "",
+    result: "",
+  };
+  const [histories, sethistory] = useState([]);
+  const [data, setData] = useState({ intialData });
+  const updateNumber1 = (e) => {
+    setData({
+      ...data,
+      number1: parseFloat(e.target.value),
+    });
+  };
+  const updateNumber2 = (e) => {
+    setData({
+      ...data,
+      number2: parseFloat(e.target.value),
+    });
+  };
+
+  const updateResult = (action) => {
+    if (action == "add") {
+      const Result = data.number1 + data.number2;
+      setData({ ...data, result: Result });
+      const newhis = data.number1 + "+" + data.number2 + "=" + Result;
+      sethistory((previoushis) => [newhis, ...previoushis]);
+    } else if (action == "sub") {
+      if (data.number1 >= data.number2) {
+        setData({ ...data, result: data.number1 - data.number2 });
+      } else {
+        alert(" number1  should be greater than number 2");
+      }
+    } else if (action == "mul") {
+      setData({ ...data, result: data.number1 * data.number2 });
+    } else if (action == "div") {
+      setData({ ...data, result: data.number1 / data.number2 });
+    } else {
+      setData(intialData);
+    }
+  };
+
+  const clearHistory = () => {
+    sethistory([]);
+  };
+
+  return (
+    <div className="row">
+      <div className="container col-md-8 md">
+        <h1>result :{data.result}</h1>
+        <label htmlFor="number1">Number 1:</label>
+        <input
+          className="m-5"
+          type="number"
+          placeholder="Enter your number"
+          id="number1"
+          value={data.number1}
+          onChange={updateNumber1}
+        />
+        <label htmlFor="number2">Number 2:</label>
+        <input
+          className="m-5"
+          type="number"
+          placeholder="Enter your number"
+          id="number2"
+          value={data.number2}
+          onChange={updateNumber2}
+        />
+
+        <div>
+          <input
+            type="button"
+            className="m-2 p-3"
+            value={"+"}
+            onClick={() => updateResult("add")}
+          />
+          <input
+            type="button"
+            className="m-2 p-3"
+            value={"-"}
+            onClick={() => updateResult("sub")}
+          />
+          <input
+            type="button"
+            className="m-2 p-3"
+            value={"*"}
+            onClick={() => updateResult("mul")}
+          />
+          <input
+            type="button"
+            className="m-2 p-3"
+            value={"/"}
+            onClick={() => updateResult("div")}
+          />
+          <input
+            type="button"
+            className="m-2 p-3"
+            value={"c"}
+            onClick={() => updateResult("c")}
+          />
+        </div>
+      </div>
+      <div className="container col-md-4">
+        <h1>history</h1>
+        <ol>
+          {histories.map((history, key) => (
+            <li key={key}>{history}</li>
+          ))}
+        </ol>
+        <input type="button" value="clear" onClick={clearHistory} />
+      </div>
+    </div>
+  );
+}
+
+export default Hello
